d30b7fe3a2db9b54e168952d6271c8b1329ae92b
hp13r <hp13rus@mail.ru>
2023-01-12 18:18:29 +0300

    Task10

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("Dima", 20, 541);
-        student.learn();
+        //Student student = new Student("Dima", 20, 541);
+        //student.learn();
+        System.out.println("Sum between a and b: " + streamSum(10000000,1000000000));
+    }
 
+    public static long streamSum (int a, int b){
+        long sumL=IntStream.range(a,b).asLongStream().sum();
+        return sumL;
     }
 }
